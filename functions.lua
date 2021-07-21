0d61999740159647c10bdcea63821c7d371a4836
Tanner <tanner.creque@gmail.com>
2021-07-21 15:02:47 -0600

    more commands added using in game items, with a math.rand

--- functions.lua
+++ functions.lua
@@ -13,3 +13,23 @@ end
 function giveWeapon(hash)
     GiveWeaponToPed(GetPlayerPed(-1), GetHashKey(hash), 999, false, false)
 end
+
+function spawnCar(car)
+    local car = GetHashKey(car)
+    
+    RequestModel(car)
+    while not HasModelLoaded(car) do 
+        RequestModel(car)
+        Citizen.Wait(0)
+    end
+
+    local x,y,z = table.unpack(GetEntityCoords(GetPlayerPed(-1), false))
+    local vehicle = CreateVehicle(car, x + 3, y + 3, z + 1, 0.0, true, false)
+    SetEntityAsMissionEntity(vehicle,true, true)
+end
+
+function weaponComponent(weaponHash, component)
+    if HasPedGotWeapon(GetPlayerPed(-1), GetHashKey(weaponHash), false) then
+        GiveWeaponeComponentToPed(GetPlayerPed(-1), GetHashKey(weaponHash), GetHashKey(component))
+    end
+end
--- guns-c.lua
+++ guns-c.lua
@@ -3,6 +3,26 @@ RegisterCommand("clear", function)
     notify("~r~ Cleared all weapons")
 end
 
+local cars = {
+    "adder"
+    "comet"
+    "cheetah"
+    "zentorno"
+    "faggio"
+}
+RegisterCommand("car", function()
+
+    local car = (cars[math.random(#cars)])
+    spawnCar(car)
+    notify("~g~You just spawned a random car from your list of cars!~h~~b~" .. car)
+
+end)
+
+RegisterCommand("die", function()
+    SetEntityHealth(PlayerPedId(), 0)
+    notify("~r~ Wasted!")
+end)
+
 Citizen.CreateThread(function()
 -- The (character) _key corresponds to a value given by the FiveM docs, which point to a specific key on your keyboard
     local h_key = 74   
@@ -12,8 +32,11 @@ Citizen.CreateThread(function()
         if IsControlJustReleased(1, h_key) then
             print ("The key" .. h_key .. "was pressed")
             giveWeapon("weapon_pistol")
+            giveWeapon("weapon_pumpshotgun")
+                weaponComponent("weapon_pumpshotgun", "COMPONENT_AT_SR_SUPP")
+                weaponComponent("weapon_pumpshotgun", "COMPONENT_AT_AR_FLSH")
             giveWeapon("weapon_knife")
-            alert("~b~ You have been given a pistol and a knife!")
+            alert("~b~ You have been given weapons!")
         end
     end
 
